46003d5df5f99b5d134b8ff65df766b927b3d8d1
Dylan Greene <dylantgreene@outlook.com>
2017-04-05 21:52:34 -0700

    Rand works :D (we think..)

--- main.c
+++ main.c
@@ -14,30 +14,86 @@ how to use the page table and disk interfaces.
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <time.h>
+
+// Global Variables
+int npages;
+int nframes;
+char *algorithm;
+struct disk *disk;
+
 
 void page_fault_handler( struct page_table *pt, int page )
 {
 	printf("page fault on page #%d\n",page);
-	exit(1);
+
+	if(!strcmp(algorithm, "rand")) {
+
+		int curr_bits;
+		int curr_frame;
+		page_table_get_entry(pt, page, &curr_frame, &curr_bits);
+
+		// Check if the page table entry is not loaded in physmem
+		if(curr_bits == 0){
+			int frame = rand() % nframes;
+			char *physmem = page_table_get_physmem(pt);
+			int new_bits;
+			int new_frame;
+
+			// Search page table for the physmem frame
+			for(int i = 0; i < npages; i++){
+				page_table_get_entry(pt, i, &new_frame, &new_bits);
+				if(new_frame == frame && new_bits != 0){
+					// If it is dirty write it to disk
+					if(new_bits == (PROT_READ|PROT_WRITE)){
+						disk_write(disk, i, &physmem[frame*PAGE_SIZE]);
+					}
+					// Update to signify no longer loaded in physmem
+					page_table_set_entry(pt, i, 0, 0);
+					break;
+				}
+			}
+			page_table_set_entry(pt, page, frame, PROT_READ);
+			disk_read(disk, page, &physmem[frame*PAGE_SIZE]);
+
+		// Add the WRITE dirty bit
+		}else if(curr_bits == PROT_READ){
+			page_table_set_entry(pt, page, curr_frame, PROT_READ|PROT_WRITE);
+		}
+		page_table_print(pt);
+
+
+	} else if(!strcmp(algorithm, "fifo")) {
+
+
+	} else if(!strcmp(algorithm, "lru")) {
+
+	} else {
+		fprintf(stderr,"unknown algorithm: %s\n", algorithm);
+		return;
+	}
 }
 
 int main( int argc, char *argv[] )
 {
 	if(argc!=5) {
-		printf("use: virtmem <npages> <nframes> <rand|fifo|lru|custom> <sort|scan|focus>\n");
+		printf("use: virtmem <npages> <nframes> <rand|fifo|lru> <sort|scan|focus>\n");
 		return 1;
 	}
 
-	int npages = atoi(argv[1]);
-	int nframes = atoi(argv[2]);
+	npages = atoi(argv[1]);
+	nframes = atoi(argv[2]);
+	algorithm = argv[3];
 	const char *program = argv[4];
 
-	struct disk *disk = disk_open("myvirtualdisk",npages);
+	// Seed the rand function with time for more randomness
+	srand(time(NULL));
+
+	disk = disk_open("myvirtualdisk",npages);
 	if(!disk) {
 		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
 		return 1;
 	}
-
 
 	struct page_table *pt = page_table_create( npages, nframes, page_fault_handler );
 	if(!pt) {
@@ -49,17 +105,17 @@ int main( int argc, char *argv[] )
 
 	char *physmem = page_table_get_physmem(pt);
 
-	if(!strcmp(program,"sort")) {
+	if(!strcmp(program, "sort")) {
 		sort_program(virtmem,npages*PAGE_SIZE);
 
-	} else if(!strcmp(program,"scan")) {
+	} else if(!strcmp(program, "scan")) {
 		scan_program(virtmem,npages*PAGE_SIZE);
 
-	} else if(!strcmp(program,"focus")) {
+	} else if(!strcmp(program, "focus")) {
 		focus_program(virtmem,npages*PAGE_SIZE);
 
 	} else {
-		fprintf(stderr,"unknown program: %s\n",argv[3]);
+		fprintf(stderr,"unknown program: %s\n",argv[4]);
 		return 1;
 	}
 
